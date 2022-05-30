1d21ba8e37a762a07064c92ca4f7fd6a3da06d5c
Eugene Kriulin <eakriulin@gmail.com>
2022-05-30 13:24:30 +0400

    Updated keywords list

--- src/dataStructures/Queue.ts
+++ src/dataStructures/Queue.ts
@@ -11,11 +11,13 @@ export class Queue<T = any> {
         return this.queue.length;
     }
 
+    // O(1) time | O(1) space
     public enqueue(value: T): number {
         this.queue.insert(value);
         return this.queue.length;
     }
 
+    // O(1) time | O(1) space
     public dequeue(): T | undefined {
         const node = this.queue.head;
         if (node) {
@@ -24,6 +26,7 @@ export class Queue<T = any> {
         }
     }
 
+    // O(1) time | O(1) space
     public peek(): T | undefined {
         const node = this.queue.head;
         return node ? node.value : undefined;
--- src/dataStructures/Stack.ts
+++ src/dataStructures/Stack.ts
@@ -9,15 +9,18 @@ export class Stack<T = any> {
         return this.stack.length;
     }
 
+    // O(1) time | O(1) space
     public push(value: T): number {
         this.stack.push(value);
         return this.stack.length;
     }
 
+    // O(1) time | O(1) space
     public pop(): T | undefined {
         return this.stack.pop();
     }
 
+    // O(1) time | O(1) space
     public peek(): T | undefined {
         const lastIndex = this.stack.length - 1;
         return this.stack[lastIndex];
