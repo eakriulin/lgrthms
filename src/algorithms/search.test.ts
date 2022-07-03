6347384621b942a3debf26d77d8f81d968d45e3d
Eugene Kriulin <eakriulin@gmail.com>
2022-07-03 18:55:35 +0400

    Quickselect Kth Largest — implementation and tests

--- src/algorithms/search.test.ts
+++ src/algorithms/search.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-magic-numbers */
-import { binarySearch, findNLargest, findNSmallest, quickselectKthSmallest, searchForRange, searchInMatrix, searchInSortedMatrix, shiftedBinarySearch } from './search';
+import { binarySearch, findNLargest, findNSmallest, quickselectKthLargest, quickselectKthSmallest, searchForRange, searchInMatrix, searchInSortedMatrix, shiftedBinarySearch } from './search';
 
 describe('binarySearch', () => {
     test('binarySearch | empty array, search for a value => undefined', () => {
@@ -425,5 +425,35 @@ describe('binarySearch', () => {
         expect(quickselectKthSmallest(objects, 3, object => object.value)).toStrictEqual({ value: 4 });
         expect(quickselectKthSmallest(objects, 4, object => object.value)).toStrictEqual({ value: 5 });
         expect(quickselectKthSmallest(objects, 5, object => object.value)).toStrictEqual({ value: 6 });
+    });
+
+    test('quickselectKthLargest | array, k is out of scope => undefined', () => {
+        const array = [4, 2, 1, 5, 6];
+        expect(quickselectKthLargest(array, -10)).toBe(undefined);
+        expect(quickselectKthLargest(array, 0)).toBe(undefined);
+        expect(quickselectKthLargest(array, 100)).toBe(undefined);
+    });
+
+    test('quickselectKthLargest | array, k is in scope => k-th smallest value returned', () => {
+        const numbers = [4, 2, 1, 5, 6];
+        expect(quickselectKthLargest(numbers, 1)).toBe(6);
+        expect(quickselectKthLargest(numbers, 2)).toBe(5);
+        expect(quickselectKthLargest(numbers, 3)).toBe(4);
+        expect(quickselectKthLargest(numbers, 4)).toBe(2);
+        expect(quickselectKthLargest(numbers, 5)).toBe(1);
+
+        const strings = ['dddd', 'b', 'aa', 'eeeee', 'ffffff'];
+        expect(quickselectKthLargest(strings, 1, string => string.length)).toBe('ffffff');
+        expect(quickselectKthLargest(strings, 2, string => string.length)).toBe('eeeee');
+        expect(quickselectKthLargest(strings, 3, string => string.length)).toBe('dddd');
+        expect(quickselectKthLargest(strings, 4, string => string.length)).toBe('aa');
+        expect(quickselectKthLargest(strings, 5, string => string.length)).toBe('b');
+
+        const objects = [{ value: 4 }, { value: 2 }, { value: 1 }, { value: 5 }, { value: 6 }];
+        expect(quickselectKthLargest(objects, 1, object => object.value)).toStrictEqual({ value: 6 });
+        expect(quickselectKthLargest(objects, 2, object => object.value)).toStrictEqual({ value: 5 });
+        expect(quickselectKthLargest(objects, 3, object => object.value)).toStrictEqual({ value: 4 });
+        expect(quickselectKthLargest(objects, 4, object => object.value)).toStrictEqual({ value: 2 });
+        expect(quickselectKthLargest(objects, 5, object => object.value)).toStrictEqual({ value: 1 });
     });
 });
--- src/algorithms/search.ts
+++ src/algorithms/search.ts
@@ -266,6 +266,48 @@ export function quickselectKthSmallest<T, K>(
     }
 }
 
+// Best: O(n) time | O(1) space
+// Avg: O(n) time | O(1) space
+// Worst: O(n^2) time | O(1) space
+export function quickselectKthLargest<T>(
+    array: T[],
+    k: number,
+): T
+export function quickselectKthLargest<T, K>(
+    array: T[],
+    k: number,
+    get: (element: T) => K,
+): T
+export function quickselectKthLargest<T, K>(
+    array: T[],
+    k: number,
+    get?: (element: T) => T | K,
+): T | undefined {
+    if (k <= 0 || k > array.length) {
+        return;
+    }
+
+    get = get ? get : (element: T): T => element;
+
+    const targetIdx = array.length - k;
+    let startIdx = 0;
+    let endIdx = array.length - 1;
+
+    while(startIdx <= endIdx) {
+        const pivotIdx = doQuicksortAndGetPivotIdx(array, get, startIdx, endIdx);
+
+        if (targetIdx === pivotIdx) {
+            return array[pivotIdx];
+        }
+
+        if (pivotIdx > targetIdx) {
+            endIdx = pivotIdx - 1;
+        } else {
+            startIdx = pivotIdx + 1;
+        }
+    }
+}
+
 function binarySearchFirstIndex<T, K>(
     array: T[],
     target: K,
